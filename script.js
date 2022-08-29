5e0df9180b3369350676eccda75481e2767d7984
Javiera-Erlandsen <javi.erlandsen98@gmail.com>
2022-08-29 18:25:38 -0400

    creacion visualizacion previa video

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".video_s").addEventListener("mouseover", function(){
+    this.play()
+})
+
+document.querySelector(".video_s").addEventListener("mouseleave", function(){
+    this.pause()
+})
